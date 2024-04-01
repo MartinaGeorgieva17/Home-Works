fac1930ae5861d7796434ecc3223c4ba658bef0c
MartinaGeorgieva17 <martina.1990.1990@gmail.com>
2024-04-01 16:24:57 +0300

    New files

---
+++ Exersise Python/Exersise Lekciq 9 29..03.2.24/Exersise 9.py
@@ -0,0 +1,98 @@
+#1.Множества...........
+#set1 = {1,2,3,3,2}
+#print(set1)
+
+
+#2. Primer..........
+#data = [1,2,3,3,2]
+#unique_data = set(data)
+#print(unique_data)
+
+
+#3. Example............
+#data = [1,2,3,3,2]
+#unique_data = list(set(data))
+#print(unique_data)
+
+
+#4. Цикъл върху сет /loop on set........................
+
+#for el in {1,2,3,3,2}:
+#    print(el)
+
+
+#5. Добавяне на елемент към множество/Add element .....................
+
+#set1 = {1,2,3,3,2}
+#set1.add(4)
+#print(set1)
+
+
+#6. Update  ..................................
+
+#set1 = {1,2,3,3,2}
+#set1.add(4)
+#set1.update([1,2,5,6])
+#print(set1)
+
+
+#7. Remove element .............................
+#set1 = {1,2,3,3,2}
+#set1.remove(2)
+#print(set1)
+
+#8. Обединение на множества /Sets Union ................
+
+#s1 = {1,2,3}
+#s2 = {2,3,4}
+
+#print(s1.|s2) 
+#print(s1.union(s2))
+
+
+#9 Сечение на две множества/ INTERSECTION..................
+#s1 = {1,2,3}
+#s2 = {2,3,4}
+#print(s1.union(s2))
+#print(s1.intersection(s2))
+
+
+#10. Разлика на две множества/DIFFERENCE........................
+
+#s1 = {1,2,3}
+#s2 = {2,3,4}
+#print(s1.difference(s2))
+#print(s2.difference(s1))
+
+
+#11. Сравняване на множества/Superset i subset ......................
+
+#s1 = {1,2,3}
+#s2 = {2,3,4}
+#print(s1.union(s2))
+#print(s1.difference(s2))
+#print(s2.difference(s1))
+
+#print (s1<s2)
+
+#Отговор: Falsh
+
+
+#12. Subset /Example (дали едното включва другото и обратното).................
+#s1 = {1,2,3,4}
+#s2 = {1,2,3,4,5}
+#print(s1<s2)
+
+#Отговор: True 
+
+
+
+#12 Тупъли...........................
+
+
+
+
+
+
+
+
