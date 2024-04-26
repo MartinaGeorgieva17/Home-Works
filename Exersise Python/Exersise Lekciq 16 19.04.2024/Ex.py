a1dde560a856ba620ca9a37902871500a88e3a63
MartinaGeorgieva17 <martina.1990.1990@gmail.com>
2024-04-26 20:53:49 +0300

    exersise

--- Exersise Python/Exersise Lekciq 16 19.04.2024/Ex.py
+++ Exersise Python/Exersise Lekciq 16 19.04.2024/Ex.py
@@ -53,3 +53,78 @@
 # a2 = A.from_dict(a2_data)
 
 # print(a2.id)
+
+
+# 3. Claa Method с конструктор, пример: @classmethod
+# METHOD TYPES 
+
+# class Spam: 
+#     def __init__ (self, id) -> None:
+#         print('Constructor method is called!')
+#         self.id = id # return self
+
+
+#     @classmethod
+#     def class_method(cls, id):
+#         print('Creted Spam object') #cls = Spam 
+#         print('Class method is called')
+#         return cls(2)
+
+#     def instance_method(self, x):
+#         print(x)
+
+
+# # create instances/objects:
+# s1 = Spam(1)
+
+# # call/invoke- class method
+# s2 = Spam.class_method(1)
+
+
+# # call instance method
+# s1.instance_method(1)
+# s2.instance_method(1)
+
+
+
+# 4. Example ....................
+
+
+# class Spam: 
+#     def __init__ (self, id) -> None:
+#         print('Constructor method is called!')
+#         self.id = id # return self
+
+
+#     @classmethod
+#     def class_method(cls, id):
+#         print('Creted Spam object') #cls = Spam 
+#         print('Class method is called')
+#         return cls(2)
+    
+#     @staticmethod
+#     def foo(x,y):
+#         print(f'Static method is called with x = {x}')
+
+
+#     def instance_method(self, x):
+#         print(x)
+
+
+# # create instances/objects:
+# s1 = Spam(1)
+
+# # call/invoke- class method
+# s2 = Spam.class_method(1)
+
+
+# # call static method
+
+# s1.foo(1)
+
+# # call instance method
+# s1.instance_method(1)
+
+
+
+
